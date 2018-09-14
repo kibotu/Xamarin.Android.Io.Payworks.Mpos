ed3f1ba181a39dda3ee93c4603221a546d636ae7
Jan Rabe <jan.rabe@exozet.com>
2018-09-14 17:43:56 +0200

    nuggetting

--- clean.sh
+++ clean.sh
@@ -1,15 +1,51 @@
 #!/usr/bin/env bash
 
-rm -rf Demo/obj
-rm -rf Demo/bin
+rm -rf AndroidSdkDemo/obj
+rm -rf AndroidSdkDemo/bin
 
-rm -rf Xamarin.Io.Payworks.Mpos.Android.Core/obj
-rm -rf Xamarin.Io.Payworks.Mpos.Android.Core/bin
+rm -rf Bolts.Android/obj
+rm -rf Bolts.Android/bin
 
-rm -rf Xamarin.Io.Payworks.Mpos.Android.Ui/obj
-rm -rf Xamarin.Io.Payworks.Mpos.Android.Ui/bin
+rm -rf Couchbase.Lite.Android/obj
+rm -rf Couchbase.Lite.Android/bin
 
-rm -rf Xamarin.Io.Payworks.Mpos.Core/obj
-rm -rf Xamarin.Io.Payworks.Mpos.Core/bin
+rm -rf Couchbase.Lite.Android.Custom/obj
+rm -rf Couchbase.Lite.Android.Custom/bin
+
+rm -rf Couchbase.Lite.Android.ForestDB/obj
+rm -rf Couchbase.Lite.Android.ForestDB/bin
+
+rm -rf Couchbase.Lite.Java.Core/obj
+rm -rf Couchbase.Lite.Java.Core/bin
+
+rm -rf Jackson.Annotations/obj
+rm -rf Jackson.Annotations/bin
+
+rm -rf Jackson.Core/obj
+rm -rf Jackson.Core/bin
+
+rm -rf Jackson.Databinding/obj
+rm -rf Jackson.Databinding/bin
+
+rm -rf LocalRepository/obj
+rm -rf LocalRepository/bin
+
+rm -rf Mpos.Android.Core/obj
+rm -rf Mpos.Android.Core/bin
+
+rm -rf Mpos.Android.Ui/obj
+rm -rf Mpos.Android.Ui/bin
+
+rm -rf Mpos.Core/obj
+rm -rf Mpos.Core/bin
+
+rm -rf Otto/obj
+rm -rf Otto/bin
+
+rm -rf StateLess4j/obj
+rm -rf StateLess4j/bin
+
+rm -rf Xamarin.Io.Payworks.Mpos.sln/obj
+rm -rf Xamarin.Io.Payworks.Mpos.sln/bin
 
 nuget restore
